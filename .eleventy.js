6aae951c8221c442204c1c89cb49793991cc4051
Jack Bradley <jack@stackupdigital.co.uk>
2022-02-10 11:10:01 +0000

    Swapped out NPM for Yarn for an easier time updating dependencies, happy to swap back if necessary. Upgraded Eleventy to v1.0.0 and identified breaking change

--- .eleventy.js
+++ .eleventy.js
@@ -1,117 +1,121 @@
 // Modules
-const readableDate = require('./_11ty/date.js');
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
-const pluginTOC = require('eleventy-plugin-nesting-toc')
-const embeds = require("eleventy-plugin-embed-everything")
-const dotenv = require('dotenv').config()
-const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
-var { DateTime } = require('luxon');
-var humanizeDuration = require('humanize-duration')
-const pluginSEO = require('eleventy-plugin-seo')
-const site = require('./src/_data/site.json')
+const readableDate = require("./_11ty/date.js");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginTOC = require("eleventy-plugin-nesting-toc");
+const embeds = require("eleventy-plugin-embed-everything");
+const dotenv = require("dotenv").config();
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+var { DateTime } = require("luxon");
+var humanizeDuration = require("humanize-duration");
+const pluginSEO = require("eleventy-plugin-seo");
+const site = require("./src/_data/site.json");
 
-console.log("Running in " + process.env.NODE_ENV + " environment...")
+console.log("Running in " + process.env.NODE_ENV + " environment...");
 
-module.exports = function(eleventyConfig) {
-
-    // Markdown
-    eleventyConfig.setLibrary(
-    'md',
-    markdownIt({ 
+module.exports = function (eleventyConfig) {
+  // Markdown
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({
       html: true,
       linkify: true,
       typographer: true,
     }).use(markdownItAnchor)
-    )
-  
-    // Layout aliases
-    eleventyConfig.addLayoutAlias('default', 'layouts/base.njk');
+  );
 
-    /* Pass through */
-    eleventyConfig.addPassthroughCopy('src/images')
-    eleventyConfig.addPassthroughCopy('src/admin')
+  // Layout aliases
+  eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
 
-    // Plugins
-    eleventyConfig.addPlugin(eleventyNavigationPlugin)
-    eleventyConfig.addPlugin(pluginTOC, {
-      tags: ['h2', 'h3'],
-      wrapper: 'nav',
-      wrapperClass: 'toc',
-    })
+  /* Pass through */
+  eleventyConfig.addPassthroughCopy("src/images");
 
-    eleventyConfig.addPlugin(embeds)
+  // @TODO - The line below causes an error due to the first point here:
+  // https://www.11ty.dev/blog/eleventy-one-point-oh/#minor
+  // https://github.com/11ty/eleventy/issues/1677
+  // It seems to think we're passing src/admin through twice.
 
-    eleventyConfig.addPlugin(pluginSEO, {
-      title: site.title,
-      description: site.description,
-      url: site.baseUrl,
-      author: site.author.name,
-      twitter: site.twitter,
-      image: site.featuredImage
-    });  
-    
+  // eleventyConfig.addPassthroughCopy('src/admin')
 
-    // Filters
-    eleventyConfig.addFilter('date', readableDate)
-    
-    // Custom inline date formatting using Luxon formats 'dd LLLL yyyy' etc
-    eleventyConfig.addFilter('dateFormat', (date, format) => {
-      return DateTime.fromISO(date).toFormat(format)
-    })
+  // Plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginTOC, {
+    tags: ["h2", "h3"],
+    wrapper: "nav",
+    wrapperClass: "toc",
+  });
 
-    // Humanly readable duration from date tag with param as end time e.g. {{ start | duration(end)}}
-    eleventyConfig.addFilter('duration', (starts, ends) => {
-      duration = DateTime.fromISO(ends).toMillis() - DateTime.fromISO(starts).toMillis()
-      return humanizeDuration(duration)
-    })
+  eleventyConfig.addPlugin(embeds);
 
-    // Limit array results
-    eleventyConfig.addFilter('limit', (arr, limit)=> {
-      return arr.slice(0, limit)
-    })
+  eleventyConfig.addPlugin(pluginSEO, {
+    title: site.title,
+    description: site.description,
+    url: site.baseUrl,
+    author: site.author.name,
+    twitter: site.twitter,
+    image: site.featuredImage,
+  });
 
+  // Filters
+  eleventyConfig.addFilter("date", readableDate);
 
-    // ... cssmin
-    // ... jsmin
+  // Custom inline date formatting using Luxon formats 'dd LLLL yyyy' etc
+  eleventyConfig.addFilter("dateFormat", (date, format) => {
+    return DateTime.fromISO(date).toFormat(format);
+  });
 
-    // Custom Collections
+  // Humanly readable duration from date tag with param as end time e.g. {{ start | duration(end)}}
+  eleventyConfig.addFilter("duration", (starts, ends) => {
+    duration =
+      DateTime.fromISO(ends).toMillis() - DateTime.fromISO(starts).toMillis();
+    return humanizeDuration(duration);
+  });
 
-    eleventyConfig.addCollection('futureEvents', (collection)=>{
-      let allEvents = collection.getFilteredByGlob("src/meetings/*.md")
-      let futureEvents = allEvents.filter( (item) => {
-        let d1 = DateTime.fromISO(item.data.start).toMillis()
-        let d2 = DateTime.now().toMillis()
-        return d1 >= d2
-      } )
-      return futureEvents
-    })
+  // Limit array results
+  eleventyConfig.addFilter("limit", (arr, limit) => {
+    return arr.slice(0, limit);
+  });
 
-    eleventyConfig.addCollection('pastEvents', (collection)=>{
-      let allEvents = collection.getFilteredByGlob("src/meetings/*.md")
-      let futureEvents = allEvents.filter( (item) => {
-        let d1 = DateTime.fromISO(item.data.start).toMillis()
-        let d2 = DateTime.now().toMillis()
-        return d1 < d2
-      } )
-      return futureEvents
-    })
+  // ... cssmin
+  // ... jsmin
 
-    // Custom Shortcodes
+  // Custom Collections
 
-    // Transforms
-    if (process.env.NODE_ENV == 'production') {
-    eleventyConfig.addTransform("htmlmin", require('./_11ty/htmlmin.js') );
-    }
+  eleventyConfig.addCollection("futureEvents", (collection) => {
+    let allEvents = collection.getFilteredByGlob("src/meetings/*.md");
+    let futureEvents = allEvents.filter((item) => {
+      let d1 = DateTime.fromISO(item.data.start).toMillis();
+      let d2 = DateTime.now().toMillis();
+      return d1 >= d2;
+    });
+    return futureEvents;
+  });
 
-    // Deep merge on front matter data such as tags
-    eleventyConfig.setDataDeepMerge(true);
-  
-    return {
-      dir: { input: 'src', output: 'dist', data: '_data' },
-      passthroughFileCopy: true,
-      templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-      htmlTemplateEngine: 'njk',
-      markdownTemplateEngine: 'njk',
-    }
+  eleventyConfig.addCollection("pastEvents", (collection) => {
+    let allEvents = collection.getFilteredByGlob("src/meetings/*.md");
+    let futureEvents = allEvents.filter((item) => {
+      let d1 = DateTime.fromISO(item.data.start).toMillis();
+      let d2 = DateTime.now().toMillis();
+      return d1 < d2;
+    });
+    return futureEvents;
+  });
+
+  // Custom Shortcodes
+
+  // Transforms
+  if (process.env.NODE_ENV == "production") {
+    eleventyConfig.addTransform("htmlmin", require("./_11ty/htmlmin.js"));
   }
+
+  // Deep merge on front matter data such as tags
+  eleventyConfig.setDataDeepMerge(true);
+
+  return {
+    dir: { input: "src", output: "dist", data: "_data" },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md", "css", "html", "yml"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
