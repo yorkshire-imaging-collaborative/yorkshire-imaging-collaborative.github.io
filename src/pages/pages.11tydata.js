5360282612143d18cde156dd0623495331e16223
Jack Bradley <jack@stackupdigital.co.uk>
2022-03-07 13:49:42 +0000

    Updated config for page support

---
+++ src/pages/pages.11tydata.js
@@ -0,0 +1,16 @@
+module.exports = {
+  eleventyComputed: {
+    eleventyNavigation: {
+      key: (data) => data.title,
+      title: (data) => data.title,
+    },
+    layout: () => "layouts/page.njk",
+    permalink: (data) => {
+      if (data.category && data.category !== "generic") {
+        return `${data.category}/${data.page.fileSlug}/index.html`;
+      } else {
+        return `/${data.page.fileSlug}/index.html`;
+      }
+    },
+  },
+};
