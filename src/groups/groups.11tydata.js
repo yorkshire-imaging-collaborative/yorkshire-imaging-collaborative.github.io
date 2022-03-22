6999f4c97fc3ba8dda02005e2d41dc5794c507dc
Jack Bradley <jack@stackupdigital.co.uk>
2022-03-22 12:20:54 +0000

    Corrected slug for title

--- src/groups/groups.11tydata.js
+++ src/groups/groups.11tydata.js
@@ -2,6 +2,7 @@ module.exports = {
   eleventyComputed: {
     eleventyNavigation: {
       parent: "Groups",
+      key: (data) => data.title,
     },
     layout: () => "layouts/sig.njk",
     tags: () => ["event"],
