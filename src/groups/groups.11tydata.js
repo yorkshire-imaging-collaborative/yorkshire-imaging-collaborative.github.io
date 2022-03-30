a16cd039385ba79d1764a6962321608d84c5f347
Jack Bradley <jack@stackupdigital.co.uk>
2022-03-30 13:02:21 +0100

    Tweaked tagging for groups to allow listing on homepage

--- src/groups/groups.11tydata.js
+++ src/groups/groups.11tydata.js
@@ -2,9 +2,13 @@ module.exports = {
   eleventyComputed: {
     eleventyNavigation: {
       parent: "Groups",
+      title: (data) => data.title,
       key: (data) => data.title,
     },
     layout: () => "layouts/sig.njk",
-    tags: () => ["event"],
+    tags: (data) => {
+      console.log(data.tags);
+      return ["event", ...data.tags];
+    },
   },
 };
