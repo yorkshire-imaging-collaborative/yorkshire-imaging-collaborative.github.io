72f293df1594c4eaf441b8fc43d3c2e1b8134d83
Jack <jack@stackupdigital.co.uk>
2022-11-09 10:02:56 +0000

    Updated special interest group function to use our netlify function - url needs updating with Ntl. Function

--- src/_data/sigmembers.js
+++ src/_data/sigmembers.js
@@ -1,8 +1,7 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function () {
-  let url =
-    "https://v1.nocodeapi.com/danfascia/airtable/EXUHjXTkuTzMBBXe?tableName=tblRVOuut4NpY7ekE&fields=Title,FullName,MembersNames,groupTag&view=ALL&perPage=all";
+  let url = "http://localhost:8888/.netlify/functions/hello-world";
 
   return (results = Cache(url, {
     duration: "1h",
