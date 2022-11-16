f15bb6f49024bbe981ee6f84234c7b2be73baa2d
Jack <jack@stackupdigital.co.uk>
2022-11-16 10:07:06 +0000

    Updated API endpoint

--- src/_data/sigmembers.js
+++ src/_data/sigmembers.js
@@ -1,7 +1,8 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function () {
-  let url = "http://localhost:8888/.netlify/functions/hello-world";
+  let url =
+    "https://yorkshire-imaging-cms.netlify.app/.netlify/functions/hello-world";
 
   return (results = Cache(url, {
     duration: "1h",
